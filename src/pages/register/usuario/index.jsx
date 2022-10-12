655ee2c430a78e2891a1f3aff053f3c218c8761f
Douglas <dodo_nadoti@hotmail.com>
2022-10-11 21:35:01 -0300

    mudo input

--- src/pages/register/usuario/index.jsx
+++ src/pages/register/usuario/index.jsx
@@ -1,6 +1,5 @@
 import * as S from './styles'
 import Link from 'next/link'
-import { Input } from '../../../components/forms/Input/Input'
 import useForm from '../../../hooks/useForm'
 
 // {
@@ -28,44 +27,10 @@ export default function RegisterUser() {
           <h2>Registra-se como Usuário</h2>
          </S.Title>
           <S.BundlerInput>
-            <Input 
-              type="text" 
-              value={email.value} 
-              onChange={email.onChange} 
-              onBlur={email.onBlur}
-              name='email'
-              label="Email"
-              error={email.error}
-            />
-            <Input 
-              type="text" 
-              value={email.value} 
-              onChange={email.onChange} 
-              onBlur={email.onBlur}
-              name='email'
-              label="Email"
-              error={email.error}
-            />
+            
           </S.BundlerInput>
           <S.BundlerInput>
-            <Input 
-              type="text" 
-              value={email.value} 
-              onChange={email.onChange} 
-              onBlur={email.onBlur}
-              name='email'
-              label="Email"
-              error={email.error}
-            />
-            <Input 
-              type="text" 
-              value={email.value} 
-              onChange={email.onChange} 
-              onBlur={email.onBlur}
-              name='email'
-              label="Email"
-              error={email.error}
-            />
+            
           </S.BundlerInput>
           
         </S.ContentLogin>
